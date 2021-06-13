427d12b733eb4cd22427631d62074b2c615000ac
Peter Rand <peterrand1112@gmail.com>
2021-06-12 22:39:05 -0600

    test

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
+import os
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1000, 600
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Light Runner")
+pygame.display.set_caption("RoadLite")
+SPACE = pygame.transform.scale(pygame.image.load(
+    os.path.join('Assets', 'space2.jpeg')), (WIDTH, HEIGHT))
+
 FPS=60 
+
+def draw_window():
+    WIN.blit(SPACE, (0,0))
+    pygame.display.update()
+
 
 def main():
     clock = pygame.time.Clock()
@@ -14,6 +23,7 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
+        draw_window()
 
 if __name__ == "__main__":
     main()
