f232060c3512977833a6a1601ad1ef03ac8518e7
Peter Rand <peterrand1112@gmail.com>
2021-06-24 13:21:45 -0600

    test

--- main.py
+++ main.py
@@ -1,29 +1,61 @@
 import pygame
 import os
+import init
+
+from pygame.constants import MOUSEBUTTONUP
+clock = pygame.time.Clock()
+
+pygame.font.init()
+pygame.mixer.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+FPS=15
+#font1 = pygame.font.Font('Assets/freesansbold.ttf', textheight)
 
 WIDTH, HEIGHT = 1000, 600
+width, height = 500, 300
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RoadLite")
-SPACE = pygame.transform.scale(pygame.image.load(
-    os.path.join('Assets', 'space2.jpeg')), (WIDTH, HEIGHT))
+sc1x0=150
 
-FPS=60 
 
-def draw_window():
-    WIN.blit(SPACE, (0,0))
+HEALTH_LOSS = pygame.USEREVENT + 1
+
+def redrawGameWindow():
+    init.WIN.blit(init.SPACE, (0,0))
+    sc1.drawChar()
+    sc2.drawChar()
+    attackmenu.make_popup()
     pygame.display.update()
 
+sc1=init.character(sc1x0,400,100,100,20,"Scorpion.png")#.drawChar()     
+sc2=init.character(650,100,200,200,10,"Scorpion.png")#.drawChar()  
 
-def main():
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-        draw_window()
+clock = pygame.time.Clock()
+run = True
 
-if __name__ == "__main__":
-    main()
+run=True
+while run:
+    OPTION=''
+    clock.tick(FPS)
+    attackmenu = init.popupmenu(50,100,200,init.textheight*2)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+            pygame.QUIT
+        if event.type == pygame.MOUSEBUTTONUP:
+            OPTION = attackmenu.option_selected()
+    #keys_pressed = pygame.key.get_pressed()
+    #if keys_pressed[pygame.K_SPACE]:  
+    #    pygame.event.post(pygame.event.Event(HEALTH_LOSS))
+    if OPTION == 'Attack':
+        sc1.x=sc1x0+100
+        if sc2.health > 1:
+            sc2.health-=1
+    else:
+        sc1.x=sc1x0
+
+    redrawGameWindow()
